e4dac2eac015cb117b27635f332b7738faf36517
Boyce Estes <estes.boyce@gmail.com>
2022-11-24 00:08:39 -0500

    Small name change for generic SheetyNavigationFlow type

--- Sources/NavigationFlow/SheetyNavigationView.swift
+++ Sources/NavigationFlow/SheetyNavigationView.swift
@@ -9,29 +9,28 @@ import SwiftUI
 import ViewInspector
 
 
-public struct SheetyNavigationView<Content: View, NavigationModalViewModel: SheetyNavigationFlow>: View {
+public struct SheetyNavigationView<Content: View, SheetyNavigationViewModel: SheetyNavigationFlow>: View {
     
-    @ObservedObject var navigationModalViewModel: NavigationModalViewModel
+    @ObservedObject var sheetyNavigationViewModel: SheetyNavigationViewModel
     
     let content: Content
     
     
-    public init(navigationModalViewModel: NavigationModalViewModel, content: Content) {
-        self.navigationModalViewModel = navigationModalViewModel
+    public init(sheetyNavigationViewModel: SheetyNavigationViewModel, content: Content) {
+        self.sheetyNavigationViewModel = sheetyNavigationViewModel
         self.content = content
     }
     
     
     public var body: some View {
         
-        content.sheet2(item: $navigationModalViewModel.modallyDisplayedView) { destination in
+        content.sheet2(item: $sheetyNavigationViewModel.modallyDisplayedView) { destination in
             
             // It actually doesn't matter where this came from,
             // we would just need to initialize this correctly from the root so that the modallyDisplayView navigationModalViewModel.displaySheet(for: destination)
             //
             // TODO: Get Presentation Indents from the viewModel if they are available
-            navigationModalViewModel.displaySheet(for: destination)
+            sheetyNavigationViewModel.displaySheet(for: destination)
         }
     }
 }
-
