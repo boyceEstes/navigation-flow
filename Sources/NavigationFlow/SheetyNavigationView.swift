cdc25b1a38d783c1638afa0f80a27e6ef13095dd
Boyce Estes <estes.boyce@gmail.com>
2022-12-13 10:00:08 -0500

    Inject onDismiss method for your sheet to provide logic after dismiss is completed

--- Sources/NavigationFlow/SheetyNavigationView.swift
+++ Sources/NavigationFlow/SheetyNavigationView.swift
@@ -12,17 +12,23 @@ public struct SheetyNavigationView<Content: View, SheetyNavigationViewModel: She
     @ObservedObject var sheetyNavigationViewModel: SheetyNavigationViewModel
     
     let content: Content
+    let onDismiss: (() -> Void)?
     
     
-    public init(sheetyNavigationViewModel: SheetyNavigationViewModel, content: Content) {
+    public init(
+        sheetyNavigationViewModel: SheetyNavigationViewModel,
+        content: Content,
+        onDismiss: (() -> Void)?
+    ) {
         self.sheetyNavigationViewModel = sheetyNavigationViewModel
         self.content = content
+        self.onDismiss = onDismiss
     }
     
     
     public var body: some View {
         
-        content.sheet2(item: $sheetyNavigationViewModel.modallyDisplayedView) { destination in
+        content.sheet2(item: $sheetyNavigationViewModel.modallyDisplayedView, onDismiss: onDismiss) { destination in
             
             // It actually doesn't matter where this came from,
             // we would just need to initialize this correctly from the root so that the modallyDisplayView navigationModalViewModel.displaySheet(for: destination)
